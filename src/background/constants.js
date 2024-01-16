f3f41abc4a9b83a5295940f4ca6ac5a7025cbe07
moris96 <moriskhoudari@gmail.com>
2024-01-16 14:59:50 -0500

    made background a little bit faster

--- src/background/constants.js
+++ src/background/constants.js
@@ -3,4 +3,4 @@ export const INPUT_RANGE_END = 1;
 export const OUTPUT_RANGE_START = -281;
 export const OUTPUT_RANGE_END = 0;
 export const ANIMATION_TO_VALUE = 1;
-export const ANIMATION_DURATION = 25000;
+export const ANIMATION_DURATION = 22500;
